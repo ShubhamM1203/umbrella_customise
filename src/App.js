239973e8d42e6151f2a1d5b7bdf6d0e47069dd97
ShubhamM1203 <96812228+ShubhamM1203@users.noreply.github.com>
2022-08-18 17:21:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Umbrella from "./screens/Umbrella";
+
+function App() {
+  return (
+    <div className="App">
+      <Umbrella />
+    </div>
+  );
+}
+
+export default App;
